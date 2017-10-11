765272db977302d6980821751ac57e32eaca98fc
jaworskm <jaworskm@advisory.com>
2017-10-11 19:12:40 -0400

    renamed from pinto to barrel

--- bin/core/terminal.js
+++ bin/core/terminal.js
@@ -4,8 +4,8 @@ module.exports = {
 
     vorpal
       .delimiter(`::}`)
-      .history(`pinto-1`)
-      .localStorage(`pinto-1`);
+      .history(`barrel-1`)
+      .localStorage(`barrel-1`);
 
     if (isInteractive) {
       vorpal.show();
