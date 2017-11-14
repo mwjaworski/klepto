7007aa6e05eb3df63fe5b3d70502f7c2abf2296d
jaws4d <jaws4d@gmail.com>
2017-11-13 21:48:34 -0500

    install local is working

--- bin/actions/install_archive_action.js
+++ bin/actions/install_archive_action.js
@@ -3,19 +3,18 @@ const ManifestConfiguration = require('../configurations/manifest')
 
 const ReferenceParser = require('../parsers/reference_parser')
 const FileSystem = require('../support/file_system')
+const StatusLog = require('../support/status_log')
 const _ = require('lodash')
 
 // const installPath = _.get(scope, 'install_to', archiveDefaultFolder)
 
 const installArchiveAction = (archiveRequest) => {
   const paths = applicationConfiguration.get(`paths`)
-  const archiveFolder = _.get(archiveRequest, 'scope.installation.destination', paths.archive)
-  const installFrom = `${archiveRequest.stagingPath}${_.get(archiveRequest, 'scope.installation.subfolder', ``)}`
   // TODO cache this?
   const manifestJson = ManifestConfiguration.build(archiveRequest.stagingPath)
-  const archivePath = `${archiveFolder}${_.get(manifestJson, 'name', archiveRequest.archive)}`
-  // TODO hide the archivePath?
-  // const archivePath = ReferenceParser.buildArchivePath(archiveRequest, manifestJson)
+  const archiveFolder = _.get(archiveRequest, 'scope.installation.destination', paths.archive)
+  const archivePath = `${archiveFolder}/${manifestJson.name || archiveRequest.archive}/`
+  const installFrom = `${archiveRequest.stagingPath}${_.get(archiveRequest, 'scope.installation.subfolder', ``)}`
   const ignoreFolders = _.merge(
     applicationConfiguration.get(`rules.ignoreFiles`, []),
     manifestJson.ignore
@@ -29,29 +28,4 @@ const installArchiveAction = (archiveRequest) => {
     .then(() => manifestJson)
 }
 
-// const installArchiveAction = (archiveRequest) => {
-//   StatusLog.notify(`install ${archiveRequest.uri}`, archiveRequest.uuid)
-//   return promoteArchiveAction(archiveRequest)
-
-//   // return downloadArchiveAction(reference)
-//   //   .then(({ archiveRequest }) => {
-//   //     StatusLog.notify(`install ${archiveRequest.uri}`, archiveRequest.uuid)
-//   //     return promoteArchiveAction(archiveRequest)
-//   //   })
-// }
-
 module.exports = installArchiveAction
-
-// StatusLog.notify(`version ${archiveRequest.uri}`, archiveRequest.uuid)
-// return VaultFacade
-//   .of(archiveRequest)
-//   .assignAppropriateVersion(archiveRequest)
-//     .then(() => {
-
-//     })
-
-// download - keep downloading
-// as you download for each dep request - add requirement to requirement tables
-// when done downloading, review conflicts
-// resolve conflicts
-// install each at one version -
